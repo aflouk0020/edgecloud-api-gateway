cffe073acd73d75271e9f311b6f6b7ae68f995f0
Taha Aflouk <tahaaflouk94@gmail.com>
2026-05-27 00:55:41 +0100

    feat(gateway): create API gateway foundation

---
+++ src/main/java/com/edgecloud/gateway/EdgecloudApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.edgecloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgecloudApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgecloudApiGatewayApplication.class, args);
+	}
+
+}
